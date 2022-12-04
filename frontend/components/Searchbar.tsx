c5c7f1739f2395f6e933a41afdedff1869892c22
RyanCodesHard <ryan.taw96@gmail.com>
2022-12-04 15:34:06 +0800

    frontend push homepage

---
+++ frontend/components/Searchbar.tsx
@@ -0,0 +1,46 @@
+import { Button, Input, InputGroup, InputLeftElement, InputRightElement, Text } from '@chakra-ui/react'
+import { CalendarIcon,SearchIcon } from '@chakra-ui/icons'
+
+export default function Searchbar(){
+	return (
+		<InputGroup
+			h='64px'
+			w='80%'
+			bg='white'
+			zIndex={3}
+			padding='0 40px'
+			borderRadius='40px'
+		>
+			<Input w='700px' variant='unstyled' placeholder='Search for your events..' />
+			<InputGroup>
+				<InputLeftElement
+					h='100%'
+					pointerEvents='none'
+					children={<CalendarIcon color='gray.200' />}
+				/>
+				<Input variant='unstyled' placeholder='Pick a Date' />
+			</InputGroup>
+			<InputGroup>
+				<InputLeftElement
+					h='100%'
+					pointerEvents='none'
+					children={<Text>$</Text>}
+				/>
+				<Input variant='unstyled' placeholder='Price range' />
+			</InputGroup>
+			<InputRightElement alignItems={'flex-end'} h='100%' w='150px'>
+				<Button 
+					leftIcon={<SearchIcon/>}
+					h='100%'
+					w='100%'
+					bg='blue'
+					color='white'
+					borderRadius='40px'
+					// onClick={handleClick}
+				>
+					Search
+				</Button>
+			</InputRightElement>
+		</InputGroup>
+	)
+}
