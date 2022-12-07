1b8cfb82d199d43077915b009b42e4a9e2aeb131
RyanCodesHard <ryan.taw96@gmail.com>
2022-12-08 00:34:54 +0800

    solana wallet integration

--- frontend/components/SolanaWalletMulti.tsx
+++ frontend/components/SolanaWalletMulti.tsx
@@ -1,12 +1,15 @@
-import { useWalletModal, WalletMultiButton } from "@solana/wallet-adapter-react-ui";
+import { useWalletModal, WalletIcon } from "@solana/wallet-adapter-react-ui";
 import { FC } from "react";
-import { Button } from '@chakra-ui/react';
+import { Button, Heading, Image } from '@chakra-ui/react';
 
 require('@solana/wallet-adapter-react-ui/styles.css');
 export const SolanaWalletMulti: FC = props => {
     const { setVisible } = useWalletModal();
 
     return (
-        <Button bg='white' w='100%' h='150px' onClick={()=>setVisible(true)}>Connect wallet</Button>
+        <Button bg='white' w='100%' h='150px' onClick={()=>setVisible(true)}>
+            <Heading>Connect wallet</Heading>
+            {/* <WalletIcon/> */}
+        </Button>
     )
 }
