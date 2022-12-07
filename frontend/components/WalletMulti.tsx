a043959eb81f099538d111299bde0a2ec22e9c2b
0-Serotonin <tomthein2@gmail.com>
2022-12-07 21:16:13 +0800

    Select Wallet button working

--- frontend/components/WalletMulti.tsx
+++ frontend/components/WalletMulti.tsx
@@ -1,13 +1,9 @@
 import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
 import { FC } from "react";
-import dynamic from "next/dynamic";
 
 export const WalletMulti: FC = props => {
-    const ReactUIWalletModalButtonDynamic = dynamic(
-    async () => (await import('@solana/wallet-adapter-react-ui')).WalletModalButton,
-    { ssr: false }
-);
+
     return (
-        <ReactUIWalletModalButtonDynamic />
+        <WalletMultiButton />
     )
 }
