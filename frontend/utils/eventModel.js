3da6b1200eca3c984c96dc71139c2b72e710e8cf
Karan <packirisamykaran@gmail.com>
2022-12-17 14:57:36 +0800

    Update eventModel.js

--- frontend/utils/eventModel.js
+++ frontend/utils/eventModel.js
@@ -4,21 +4,21 @@ const Schema = mongoose.Schema;
 const eventSchema = new Schema({
 	"Name of event": String, //Elon musk firing party
 	"Event Description": String, //Welcome to the mass firing event of Twitter hosted by the almighty Elon Musk.
-	Category: String;
+	Category: Sting,
 	walletAddress: String, //AD23vfW14
 	"Start Datetime": String, //idk what format String or date in mongodb, this will include start time also
 	"End Datetime": String, //same as start, with time also
 	Location: String, //Maybe postal code or some other identifiers as fields might be better
 	"Organizers Email": String, //elonmusk@gmail.com
-	Organizer: String;
+	Organizer: Sting,
 	"Event Capacity": Number, //2000
 	"Ticket price": Number, //7.99
 	"Ticket Image": String, //a url? or a Base64 String as img
-	"Background Image": String;
+	"Background Image": Sting,
 	EventId: String, //ASDL124Vfrfw3
 	active: Boolean, //if event is currently active,
-	candyMachineId: String;
-	collectionId: String;
+	candyMachineId: Sting,
+	collectionId: Sting,
 });
 
 module.exports = mongoose.models.event || mongoose.model("event", eventSchema);
