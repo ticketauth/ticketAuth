7c74d14390c3b8d2ab3fe38a0a67193bb6985da8
Karan <packirisamykaran@gmail.com>
2022-12-17 14:55:26 +0800

    model change

--- frontend/utils/eventModel.js
+++ frontend/utils/eventModel.js
@@ -2,22 +2,23 @@ import mongoose from "mongoose";
 const Schema = mongoose.Schema;
 
 const eventSchema = new Schema({
-	"Name of event": String,
-	EventId: String,
-	"Event Description": String,
-	walletAddress: String,
-	"Start Datetime": String,
-	"End Datetime": String,
-	Location: String,
-	Coordinates: { lat: Number, lng: Number },
-	"Organizers Email": String,
-	"Event Capacity": Number,
-	"Ticket price": Number,
-	"Ticket Image": String,
-	"Background Image": String,
-	active: Boolean, //if event is currently active
-	CollectionId: String,
-	CandyMachineId: String,
+	"Name of event": String, //Elon musk firing party
+	"Event Description": String, //Welcome to the mass firing event of Twitter hosted by the almighty Elon Musk.
+	Category: String;
+	walletAddress: String, //AD23vfW14
+	"Start Datetime": String, //idk what format String or date in mongodb, this will include start time also
+	"End Datetime": String, //same as start, with time also
+	Location: String, //Maybe postal code or some other identifiers as fields might be better
+	"Organizers Email": String, //elonmusk@gmail.com
+	Organizer: String;
+	"Event Capacity": Number, //2000
+	"Ticket price": Number, //7.99
+	"Ticket Image": String, //a url? or a Base64 String as img
+	"Background Image": String;
+	EventId: String, //ASDL124Vfrfw3
+	active: Boolean, //if event is currently active,
+	candyMachineId: String;
+	collectionId: String;
 });
 
 module.exports = mongoose.models.event || mongoose.model("event", eventSchema);
