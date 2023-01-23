b9ada94bdeaf58170a27ff5c18b3ef49e184a3a1
Karan <packirisamykaran@gmail.com>
2023-01-23 19:26:52 +0800

    Update candyMachineInterfaces.ts

--- frontend/utils/dataInterfaces/candyMachineInterfaces.ts
+++ frontend/utils/dataInterfaces/candyMachineInterfaces.ts
@@ -2,15 +2,15 @@ import { WalletContextState } from '@solana/wallet-adapter-react';
 import { type Connection } from '@solana/web3.js';
 
 export interface CandyMachineData {
-  'Name of event': string;
-  'Event Description': string;
-  'Start Event Datetime': string;
-  'End Event Datetime': string;
-  'Start Sale Datetime': string;
-  'End Sale Datetime': string;
-  'Event Capacity': number;
-  'Ticket price': number;
-  ticketFile: File;
-  wallet: WalletContextState;
-  connection: Connection;
+  EventName: string;
+  EventDescription: string;
+  StartEventDatetime: string;
+  EndEventDatetime: string; //same as start, with time also
+  StartSaleDatetime: string;
+  EndSaleDatetime: string;
+  EventCapacity: number;
+  TicketPrice: number;
+  TicketFile: File;
+  Wallet: WalletContextState;
+  Connection: Connection;
 }
